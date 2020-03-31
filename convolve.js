406be046ec240b936ff3d198e25f12e70d149073
Nick Dulworth <ndulworth@gmail.com>
2020-03-31 01:13:04 -0500

    Update convolve.js

--- convolve.js
+++ convolve.js
@@ -226,13 +226,12 @@
     imgElement.src = images[impulse]; // follows selected impulse
 
     // imageURL = images[impulse];
+    if impulse == 0 {document.body.style.backgroundImage = "url(images/0.jpg)";}
 
-    if impulse == 0 {document.body.style.backgroundImage = "url(images/0.jpg)";
-    }
-    else if impulse == 1 {document.body.style.backgroundImage = "url(images/1.jpg)";
-    } 
-    else if impulse == 2 {document.body.style.backgroundImage = "url(images/2.jpg)";
-    } 
+    else if impulse == 1 {document.body.style.backgroundImage = "url(images/1.jpg)";}
+
+    else if impulse == 2 {document.body.style.backgroundImage = "url(images/2.jpg)";} 
+    
     else {}
 
   }
