f7c2dff9eaf62827581c561a1d00b79e59c41ae6
Nick Dulworth <ndulworth@gmail.com>
2020-04-11 17:33:08 -0500

    hi

--- convolve.js
+++ convolve.js
@@ -215,6 +215,7 @@ var isBlink = (isChrome || isOpera) && !!window.CSS;
     // Play it.
     audioContext.resume();
     recordingNode.start();
+    console.log('Preview Recording');
   }
 
   function handleToggleRecording() {
